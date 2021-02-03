eb97f8154675d5e59120151d0990806a76b1654e
Thi Vieira <thiiagoviieira@gmail.com>
2021-02-03 16:36:16 -0300

    Depois de muita pesquisa e trabalho, exercicio 1 finalizado rs!

--- src/Exercicio1/Pessoa.java
+++ src/Exercicio1/Pessoa.java
@@ -1,15 +1,14 @@
 package Exercicio1;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Pessoa {
     private String nome;
-    private Date dataNascimento;
+    private LocalDate dataNascimento;
     private double altura;
 
-    public Pessoa(String nome, Date dataNascimento, double altura) {
+    public Pessoa(String nome, LocalDate dataNascimento, double altura) {
         this.nome = nome;
         this.dataNascimento = dataNascimento;
         this.altura = altura;
@@ -23,11 +22,11 @@ public class Pessoa {
         this.nome = nome;
     }
 
-    public Date getDataNascimento() {
+    public LocalDate getDataNascimento() {
         return dataNascimento;
     }
 
-    public void setDataNascimento(Date dataNascimento) {
+    public void setDataNascimento(LocalDate dataNascimento) {
         this.dataNascimento = dataNascimento;
     }
 
@@ -39,30 +38,32 @@ public class Pessoa {
         this.altura = altura;
     }
 
-    public static void calculaIdade(Date dataRecebida){
-//        Calendar dataAtual = Calendar.getInstance();
-//        System.out.printf("%tD\n", dataAtual);
+//    public static void calculaIdade(Date dataRecebida) {
+////        Calendar dataAtual = Calendar.getInstance();
+////        System.out.printf("%tD\n", dataAtual);
+//        LocalDate dataNascimento = LocalDate.of(1998,1,13);
+//        int idade = Pessoa.calculateAge(dataNascimento, LocalDate.of(2021,2,21));
+//    }
 
-        Date dataAtual = new Date();
-        System.out.println(dataAtual.);
-
-        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-
-        int idade = 0;
-        System.out.println("Idade: " + idade);
+    public static int calculateAge(LocalDate birthDate, LocalDate currentDate) {
+        if ((birthDate != null) && (currentDate != null)) {
+            return Period.between(birthDate, currentDate).getYears();
+        } else {
+            return 0;
+        }
     }
 
-    public static void exibePessoa(Pessoa pessoa){
+    public static void exibePessoa(Pessoa pessoa) {
         System.out.println("Nome: " + pessoa.getNome());
         System.out.println("Altura: " + pessoa.getAltura() + " cm");
         System.out.println("Data de nascimento: " + pessoa.getDataNascimento());
-        calculaIdade(pessoa.getDataNascimento());
-//        System.out.println("Idade: " + idade);
+        int idade = Pessoa.calculateAge(pessoa.getDataNascimento(), LocalDate.now());
+        System.out.println("Idade: " + idade);
     }
 
     public static void main(String[] args) {
-        Date idade = new Date(1998 - 1900,0,13);
-        Pessoa clienteUm = new Pessoa("Thiago", idade,1.92);
+//        LocalDate dataNascimento = LocalDate.of(1998,1,13);
+        Pessoa clienteUm = new Pessoa("Thiago", LocalDate.of(1998,1,13), 1.92);
         exibePessoa(clienteUm);
     }
 }
